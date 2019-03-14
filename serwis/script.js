fcfe87db70fed96707955861d7bc40aa098500e4
KennyDaktyl <krakow@miktelgsm.pl>
2019-03-14 08:09:50 +0100

    Dodano podstrone serwis

--- serwis/script.js
+++ serwis/script.js
@@ -1,23 +1,18 @@
-//WYSWIETLANIE OFERTY
-const bt0 = document.querySelector(".motto");
-const bt1 = document.querySelector(".item1");
-const bt11 = document.querySelector(".oferta1");
-const bt2 = document.querySelector(".item2");
-const bt22 = document.querySelector(".oferta2");
-const bt3 = document.querySelector(".item3");
-const bt33 = document.querySelector(".oferta3");
-const bt4 = document.querySelector(".item4");
-const bt44 = document.querySelector(".oferta4");
-const bt5 = document.querySelector(".item5");
-const bt55 = document.querySelector(".oferta5");
-const bt6 = document.querySelector(".item6");
-const bt66 = document.querySelector(".oferta6");
-const bt7 = document.querySelector(".item7");
-const bt77 = document.querySelector(".oferta7");
-const bt8 = document.querySelector(".item8");
-const bt9 = document.querySelector(".item9");
+const menu = document.querySelector(".navi");
+const open = document.querySelector(".fa-bars");
+const close = document.querySelector(".fa-window-close");
 
-const of0 = document.querySelector(".of0");
+const nav1 = document.querySelector(".lcd");
+const nav2 = document.querySelector(".dotyk");
+const nav3 = document.querySelector(".złącza");
+const nav4 = document.querySelector(".zalane");
+const nav5 = document.querySelector(".android");
+const nav6 = document.querySelector(".buzzer");
+const nav7 = document.querySelector(".microfon");
+const nav8 = document.querySelector(".data");
+
+const front = document.querySelector(".front");
+
 const of1 = document.querySelector(".of1");
 const of2 = document.querySelector(".of2");
 const of3 = document.querySelector(".of3");
@@ -25,113 +20,110 @@ const of4 = document.querySelector(".of4");
 const of5 = document.querySelector(".of5");
 const of6 = document.querySelector(".of6");
 const of7 = document.querySelector(".of7");
-const of8 = document.querySelector(".of7");
-const of9 = document.querySelector(".of7");
+const of8 = document.querySelector(".of8");
 
 
+open.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.remove("active")
+    close.classList.add("active")
+    front.classList.add("active")
+    front.classList.remove("none")
+    of1.classList.remove("active")
+    of2.classList.remove("active")
+    of3.classList.remove("active")
+    of4.classList.remove("active")
+    of5.classList.remove("active")
+    of6.classList.remove("active")
+    of7.classList.remove("active")
+    of8.classList.remove("active")
+})
 
-bt1.addEventListener("click", function () {
-    of0.classList.add("none")
+close.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.remove("active")
+    of1.classList.remove("active")
+    of2.classList.remove("active")
+    of3.classList.remove("active")
+    of4.classList.remove("active")
+    of5.classList.remove("active")
+    of6.classList.remove("active")
+    of7.classList.remove("active")
+    of8.classList.remove("active")
+})
+
+nav1.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.add("active")
     of2.classList.remove("active")
     of3.classList.remove("active")
     of4.classList.remove("active")
     of5.classList.remove("active")
+    of6.classList.remove("active")
+    of7.classList.remove("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
 })
 
-
-// bt11.addEventListener("click", function () {
-//     of0.classList.add("none")
-//     of1.classList.add("active")
-//     of2.classList.remove("active")
-//     of3.classList.remove("active")
-//     of4.classList.remove("active")
-//     of5.classList.remove("active")
-//     of8.classList.remove("active")
-//     of9.classList.remove("active")
-// })
-
-bt2.addEventListener("click", function () {
-    of0.classList.add("none")
+nav2.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.add("active")
     of3.classList.remove("active")
     of4.classList.remove("active")
     of5.classList.remove("active")
+    of6.classList.remove("active")
+    of7.classList.remove("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
 })
 
-bt22.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.add("active")
-    of3.classList.remove("active")
-    of4.classList.remove("active")
-    of5.classList.remove("active")
-    of8.classList.remove("active")
-    of9.classList.remove("active")
-})
-
-bt3.addEventListener("click", function () {
-    of0.classList.add("none")
+nav3.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.remove("active")
     of3.classList.add("active")
     of4.classList.remove("active")
     of5.classList.remove("active")
+    of6.classList.remove("active")
+    of7.classList.remove("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
 })
 
-bt33.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.remove("active")
-    of3.classList.add("active")
-    of4.classList.remove("active")
-    of5.classList.remove("active")
-    of8.classList.remove("active")
-    of9.classList.remove("active")
-})
-
-bt4.addEventListener("click", function () {
-    of0.classList.add("none")
+nav4.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.remove("active")
     of3.classList.remove("active")
     of4.classList.add("active")
     of5.classList.remove("active")
+    of6.classList.remove("active")
+    of7.classList.remove("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
 })
 
-bt44.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.remove("active")
-    of3.classList.remove("active")
-    of4.classList.add("active")
-    of5.classList.remove("active")
-    of8.classList.remove("active")
-    of9.classList.remove("active")
-})
-
-bt5.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.remove("active")
-    of3.classList.remove("active")
-    of4.classList.remove("active")
-    of5.classList.add("active")
-    of8.classList.remove("active")
-    of9.classList.remove("active")
-})
-
-bt55.addEventListener("click", function () {
-    of0.classList.add("none")
+nav5.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.remove("active")
     of3.classList.remove("active")
@@ -140,37 +132,32 @@ bt55.addEventListener("click", function () {
     of6.classList.remove("active")
     of7.classList.remove("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
 })
 
-bt6.addEventListener("click", function () {
-    of0.classList.add("none")
+nav6.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.remove("active")
     of3.classList.remove("active")
     of4.classList.remove("active")
     of5.classList.remove("active")
-    of6.classList.add("active")
-    of7.classList.remove("active")
-    of8.classList.remove("active")
-    of9.classList.remove("active")
-})
-
-bt66.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.remove("active")
-    of3.classList.remove("active")
-    of4.classList.remove("active")
+    of6.classList.remove("active")
     of5.classList.remove("active")
     of6.classList.add("active")
     of7.classList.remove("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
 })
 
-bt7.addEventListener("click", function () {
-    of0.classList.add("none")
+nav7.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.remove("active")
     of3.classList.remove("active")
@@ -179,115 +166,20 @@ bt7.addEventListener("click", function () {
     of6.classList.remove("active")
     of7.classList.add("active")
     of8.classList.remove("active")
-    of9.classList.remove("active")
-
-
 })
 
-bt77.addEventListener("click", function () {
-    of0.classList.add("none")
+nav8.addEventListener("click", function () {
+    menu.classList.toggle("active")
+    open.classList.add("active")
+    close.classList.remove("active")
+    front.classList.add("none")
+    front.classList.remove("active")
     of1.classList.remove("active")
     of2.classList.remove("active")
     of3.classList.remove("active")
     of4.classList.remove("active")
     of5.classList.remove("active")
-    of6.classList.remove("active")
-    of7.classList.add("active")
-    of8.classList.remove("active")
-    of9.classList.remove("active")
-
-})
-
-bt8.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.remove("active")
-    of3.classList.remove("active")
-    of4.classList.remove("active")
-    of5.classList.remove("active")
-    of6.classList.remove("active")
     of6.classList.remove("active")
     of7.classList.remove("active")
     of8.classList.add("active")
-    of9.classList.remove("active")
-
-
 })
-
-bt9.addEventListener("click", function () {
-    of0.classList.add("none")
-    of1.classList.remove("active")
-    of2.classList.remove("active")
-    of3.classList.remove("active")
-    of4.classList.remove("active")
-    of5.classList.remove("active")
-    of6.classList.remove("active")
-    of7.classList.remove("active")
-    of8.classList.remove("active")
-    of9.classList.add("active")
-})
-
-//SCROLLOWANIE PO WYBORZE
-$('.item1').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s1').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item2').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s2').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item3').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s3').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item4').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s4').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item5').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s5').offset().top - $('.s0').offset().top
-    })
-})
-
-
-$('.item6').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s6').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item7').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s7').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item8').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s8').offset().top - $('.s0').offset().top
-    })
-})
-
-$('.item9').on('click', function () {
-    $('body,html').animate({
-        scrollTop: $('.s9').offset().top - $('.s0').offset().top
-    })
-})
-
-
-//Margin top dla artukułow
-
-// const heightTyt = document.querySelector(".tytul");
-// const marTopMain = document.querySelector(".main");
-
-var a = $('.tytul').height();
-$('.main').css('margin-top', a);
